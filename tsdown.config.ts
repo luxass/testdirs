dc8f3082725823efd9d756ccacc28a0a5fc7ce3e
Lucas <lucasnrgaard@gmail.com>
2025-05-11 09:26:54 +0200

    feat!: drop support for cjs

---
+++ tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "tsdown";
+
+export default defineConfig({
+  entry: [
+    "./src/index.ts",
+    "./src/helpers.ts",
+  ],
+  format: "esm",
+  clean: true,
+  dts: true,
+  treeshake: true,
+  publint: true,
+});
